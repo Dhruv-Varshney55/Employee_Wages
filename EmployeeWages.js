3a2cbae51f18eae5a8eededd475294f03fcb86a4
Dhruv-Varshney55 <dhruvvarshney24112002@gmail.com>
2025-03-21 11:14:40 +0530

    UC2

--- EmployeeWages.js
+++ EmployeeWages.js
@@ -1,4 +1,4 @@
-// UC1 - Check Employee Presence
+// UC1 (Check Employee Presence)
 
 const present = 1;
 let empCheck = Math.floor(Math.random() * 10) % 2;
@@ -9,3 +9,31 @@ if(empCheck == present){
 else{
     console.log("Employee is Absent");
 }
+
+
+
+
+
+// UC2 (Calculate daily employee wage)
+
+let hours = Math.floor(Math.random() * 10) % 3;
+
+const isPartTime = 1;
+const isFullTime = 2;
+const partTime = 4;
+const fullTime = 8;
+const wagePerHour = 20;
+
+switch(hours){
+    case isPartTime:
+        hours = partTime;
+        break;
+    case isFullTime:
+        hours = fullTime;
+        break;
+    default:
+        hours = 0;
+}
+
+let wage = wagePerHour * hours;
+console.log("Daily wage:", wage);
